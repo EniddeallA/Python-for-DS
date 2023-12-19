da6282bfa054f17e115ed16cccd29cd32b7639ff
Allaeddine Khalid <allaeddinekhalid@gmail.com>
2023-12-19 17:25:01 +0100

    progress

--- day04/ex03/new_student.py
+++ day04/ex03/new_student.py
@@ -4,18 +4,31 @@ from dataclasses import dataclass, field
 
 
 def generate_id() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    """Generate random id"""
+    return "".join(random.choices(string.ascii_lowercase, k=15))
 
 
 @dataclass
 class Student:
-    pass
+    """Student dataclass"""
+    name: str
+    surname: str
+    active: bool = True
+    login: str = field(init=False)
+    id: str = field(default_factory=generate_id, init=False)
+
+    def __post_init__(self):
+        self.login = self.name[0].upper() + self.surname.lower()
 
 
 def main():
-    student = Student(name = "Edward", surname = "agle")
+    student = Student(name="Edward", surname="agle")
     print(student)
-    
+    student1 = Student(name="Edward", surname="agle", active=False)
+    print(student1)
+    # student2 = Student(name="Edward", surname="agle",  id = "toto")
+    # print(student2)
+
 
 if __name__ == "__main__":
     main()
